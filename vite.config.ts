ff0e413b377a4525808a5071e37648604d000281
John_Soi <un.perso@yandex.ru>
2026-02-23 16:12:19 +0500

    https://github.com/users/JohnSoi/projects/3/views/1?pane=issue&itemId=158885362&issue=JohnSoi%7Cedu-pilot-frontend%7C1

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
+import { UserConfig } from 'vite';
+
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '')
 
@@ -28,12 +30,6 @@ export default defineConfig(({ mode }) => {
             assetsDir: 'assets',
             sourcemap: mode === 'development',
             minify: 'terser',
-            terserOptions: {
-                compress: {
-                    drop_console: mode === 'production',
-                    drop_debugger: mode === 'production',
-                },
-            },
             rollupOptions: {
                 output: {
                     manualChunks: {
