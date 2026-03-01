51b662c5e9de7280c05cd5cd4bf7f4430bf8b261
JohnSoi <un.perso@yandex.ru>
2026-03-01 09:02:08 +0500

    https://github.com/users/JohnSoi/projects/3/views/1?pane=issue&itemId=158888057&issue=JohnSoi%7Cedu-pilot-frontend%7C4

--- src/services/consts.ts
+++ src/services/consts.ts
@@ -1,3 +1,13 @@
+/**
+ * @enum {string} MethodType
+ * @description Перечисление HTTP-методов, поддерживаемых сервисом.
+ * 
+ * @property {string} GET - Получение данных
+ * @property {string} POST - Создание данных
+ * @property {string} PUT - Полное обновление данных
+ * @property {string} PATCH - Частичное обновление данных
+ * @property {string} DELETE - Удаление данных
+ */
 enum MethodType {
     GET = "GET",
     POST = "POST",
@@ -6,9 +16,29 @@ enum MethodType {
     DELETE = "DELETE"
 }
 
+/**
+ * @constant {string} BASE_URL
+ * @description Базовый URL-адрес сервера API по умолчанию.
+ * @default "http://localhost:8000"
+ */
 const BASE_URL: string = "http://localhost:8000";
+/**
+ * @constant {string} BASE_PROTOCOL
+ * @description Протокол по умолчанию для запросов к API.
+ * @default "https"
+ */
 const BASE_PROTOCOL: string = "https";
+/**
+ * @constant {number} BASE_TIMEOUT
+ * @description Таймаут запроса по умолчанию в миллисехундах.
+ * @default 15000
+ */
 const BASE_TIMEOUT: number = 15_000;
+/**
+ * @constant {string} TOKEN_KEY_NAME
+ * @description Ключ для хранения токена аутентификации в localStorage.
+ * @default "edu_pilot_sid"
+ */
 const TOKEN_KEY_NAME: string="edu_pilot_sid";
 
 export { MethodType, BASE_URL, BASE_PROTOCOL, BASE_TIMEOUT, TOKEN_KEY_NAME }
